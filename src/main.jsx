9692771ab4d330aedd61127f4e8a72e9a369aafb
Julianna Lamm <juliannalamm@gmail.com>
2025-04-05 14:44:30 -0400

    imported fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './fonts.css';    // Import fonts first so they load globally
 import './index.css'
 import App from './App.jsx'
 
