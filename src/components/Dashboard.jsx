fd797407cec7cdee745c7f2171051b60f742bd45
Julianna Lamm <juliannalamm@gmail.com>
2025-04-10 17:47:23 -0400

    changed bg color

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,12 +4,12 @@ import DashboardClusterChart from './Charts/DashboardClusterChart';
 const Dashboard = () => {
   return (
     <section 
-      className="w-full min-h-screen bg-burgundy flex flex-col items-center justify-center py-16"
+      className="w-full min-h-screen bg-lightblue flex flex-col items-center justify-center py-16"
     >
       <h1 className="text-6xl font-bold text-white mb-8 text-center">
         Interactive Clustering Dashboard
       </h1>
-      <div className="w-full max-w-screen-xl mx-auto bg-white p-8">
+      <div className="w-full max-w-screen-xl mx-auto bg-transparent p-8">
         <DashboardClusterChart />
         
       </div>
